ae0496693e7551e6c2872f422f8295d87dafd4a7
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-01-29 12:12:53 -0800

    Update and rename 01.29 wk4_late.DOCX to 01.29 wk4_late.cpp

---
+++ Notes/01.29 wk4_late.cpp
@@ -0,0 +1,8 @@
+//NEED TO READ #1&4
+
+//3. Pointer Variable - combined variable deriving from basic variables
+int *ptr;
+int* ptr;
+int *ptr, *b;
+
+ptr = &a;  //can work, but better in two steps instead of one
