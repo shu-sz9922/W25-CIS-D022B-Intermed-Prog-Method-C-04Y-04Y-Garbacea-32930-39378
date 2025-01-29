c04b0ec3181683272a6842d09fe034317da35853
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-01-29 12:39:25 -0800

    Create corrections.cpp

---
+++ Notes/corrections.cpp
@@ -0,0 +1,3 @@
+//3.1: Grouping data: struct
+// 3.1.4: The struct construct.
+// 1)A struct definition for CartesianPoint has subitems int x and int y. How many int locations in memory does the struct definition allocate?
