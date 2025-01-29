4822b99b4bc7efce8bc984dbbd64b8f12c0e80b5
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-01-29 12:30:55 -0800

    Update 01.29 wk4_late.cpp

--- Notes/01.29 wk4_late.cpp
+++ Notes/01.29 wk4_late.cpp
@@ -6,3 +6,40 @@ int* ptr;
 int *ptr, *b;
 
 ptr = &a;  //can work, but better in two steps instead of one
+
+
+//4. The address (&) and indirection (*) operators
+int  a = 10, b;
+int *ptr; 
+
+ptr = &a;
+
+cin >> a;
+b = a + 1; 
+a++;
+
+
+//5. What is wrong?
+double  pi = 3.14; 
+double *ptr; //can be double since pi is a double - see exL32
+
+cout << pi   << endl;
+cout << &ptr << endl;
+cout <<  ptr << endl;
+cout << *ptr << endl; //couldn't work since it hasn't been initialize
+
+//22A - char can be converted into int
+//ex: pic 12:23
+
+
+//A pointer variable is a variable that can store an address.
+double  n = 75.99;
+double *ptr;
+
+ptr  = &n;
+
+cout << n << " " << &n  << " " << ptr;
+//in the past, we could learn levels of ptr
+//q = &ptr;
+//then how can we define q?
+// double **q;
